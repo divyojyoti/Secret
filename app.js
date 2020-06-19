b893eb17853d461ed83c0dcb89514f381a7c622c
Divyojyoti Ghosh <divyosmiley9@gmail.com>
2020-06-19 12:45:30 +0530

    Encryption Level 1 & 2

---
+++ app.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require( "body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require('mongoose-encryption')
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true} );
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET_KEY, encryptedFields:["password"]});
+
+const User = mongoose.model("User", userSchema);
+app.get("/",function(req, res){
+    res.render("home");
+});
+
+app.route("/login")
+
+    .get( function (req, res) {
+        res.render("login");
+    })
+    
+    .post(function(req,res){
+        User.find({
+          email: req.body.username,
+          password: req.body.password,
+        }, function(err, foundUser){
+            if(!err){
+                if(foundUser)
+                res.render("secrets")
+             else
+                res.redirect("/login")  
+            }
+             
+            }
+        )
+    });
+
+app.route("/register")
+
+.get( function (req, res) {
+  res.render("register");
+})
+
+.post(function(req, res){
+    const newUser= new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+    newUser.save(function(err){
+        if(!err)
+        res.render("secrets")
+        else
+        console.log(err);
+        
+    });
+});
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server started at port 3000");
+})
